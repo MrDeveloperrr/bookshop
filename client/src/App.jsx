4135507d8e7666ff61cde12c5f4a3829a2198092
MrDeveloperrr <mrdemote@gmail.com>
2024-04-18 15:47:48 -0400

    bookshop update

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className='text-amber-500 text-9xl text-center'>welcome to the store</h1>
+    </>
+  )
+}
+
+export default App
